7acc1bcabd11b8a3c2d5c298c6688c2aa19611f3
sachindrakarki2 <sachindrakarki2@gmail.com>
2022-10-17 14:04:23 +1100

    Data man string

---
+++ style.js
@@ -0,0 +1,36 @@
+// === String
+
+// const str = "Hi there, My name is Prem, and i live in Sydney.";
+
+// const val = str.length;
+// const val = str.toUpperCase();
+// const val = str.toLowerCase();
+// const val = str.indexOf("i");
+// const val = str.lastIndexOf("i");
+// const val = str.replace("i", "you");
+// const val = str.replaceAll("Prem", "you");
+// const val = str.substring(5, 13);
+// const val = str.slice(5, 13);
+// const val = str.slice(5);
+// const val = str.slice(-5);
+
+// const val = str.includes("Prem");
+// const val = str.charAt(1);
+// const val = str.charCodeAt(10);
+
+// const val = str.charCodeAt(10);
+
+// const str = 'Hi there, My "name" is Prem, and i live in Sydney.';
+
+// const val = str.split(",");
+// const val = str.search("sdfname");
+// console.log(typeof val);
+// console.log(val);
+
+// Escape character
+// Length
+// indexOf, lastIndexOf, search
+// Slice, substring, substr
+// Replace, contact, trim
+// toUpperCase, toLowerCase
+// charAt, [], split
